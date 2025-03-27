55b14fe5ee8daa44cd1a73d78fe9156bc7ef019c
thienisme2004 <thienisme2004@gmail.com>
2025-03-28 01:03:28 +0700

    aaaa

---
+++ Cau.sql
@@ -0,0 +1,20 @@
+update sanpham set mota = 'High-quality helmet, safe and stylish.'
+update sanpham set tensanpham = 'Helmet'
+
+update magiamgia set tenmagiamgia = '10% discount on the total order value.'
+update magiamgia set tenmagiamgia = 'Free shipping.'
+
+
+select * from sanpham
+select * from sanpham_size
+select * from donhang
+select * from chitietdonhang
+select * from khachhang
+select * from magiamgia
+select * from khachhang_magiamgia
+select * from categories
+select * from danhgiasanpham
+
+
+
+
